78a4576d2ccd0d1a893fab0962c6b8ab7c27599d
Jaden Hou <jadenhou19@gmail.com>
2023-10-17 20:12:05 -0400

    lint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
-  import "../app.css";
-  import Navbar from "$lib/components/Navbar/Navbar.svelte";
+	import '../app.css';
+	import Navbar from '$lib/components/Navbar/Navbar.svelte';
 </script>
+
 <Navbar />
 <slot />
