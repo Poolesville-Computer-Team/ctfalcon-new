3d52054a70d321ed82cd9136b40182fcd2e7891c
Jaden Hou <jadenhou19@gmail.com>
2023-10-18 22:03:30 -0400

    fixed bugs with navbar changed colors and font

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -4,25 +4,22 @@
 	let active: boolean = false;
 </script>
 
-<nav class={`${active ? 'h-screen' : 'h-12'} w-screen`}>
+<nav class={`${active ? 'h-screen' : 'h-16'} w-screen`}>
 	<div
-		class={`mx-auto flex h-full w-11/12 max-w-4xl ${
-			active ? 'items-start' : 'items-center'
-		} justify-between text-xl`}
+		class={`mx-auto flex h-full w-11/12 max-w-4xl items-start justify-between text-lg font-poppins md:items-center`}
 	>
-		<div class="flex h-12 items-center"><a href="/">CTFalcon</a></div>
+		<div class="flex h-16 items-center"><a href="/">CTFalcon</a></div>
 		<ul
-			class={`mt-12 flex flex-col items-center gap-8 overflow-hidden md:mt-0 md:flex-row ${
-				active ? 'h-full' : 'h-0 md:h-12'
+			class={`mt-12 flex flex-col items-center gap-8 overflow-hidden transition-[height] md:mt-0 md:h-16 md:flex-row ${
+				active ? 'h-screen' : 'h-0'
 			}`}
 		>
 			<li><a href="/">Home</a></li>
-			<li><a href="/rules">Rules</a></li>
-			<li><a href="/events">Events</a></li>
-			<li><a href="/challenges">Leaderboard</a></li>
+			<li><a href="/rules">Challenges</a></li>
+			<li><a href="/events">About</a></li>
 			<li class="md:hidden"><a href="/login">Login</a></li>
 		</ul>
-		<div class="flex h-12 items-center">
+		<div class="flex h-16 flex-col items-center justify-center">
 			<a href="/login" class="hidden md:block">Login</a>
 			<div class="md:hidden"><Hamburger bind:active /></div>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'dark-blue': '#060E13'
+			},
+            fontFamily: {
+                'sans': ['Inter', 'sans-serif'],
+                'poppins': ['Poppins', 'sans-serif']
+            }
+		}
 	},
 	plugins: []
 };
